39a8cbeb9230b9487e88926b6ace749a0236aa50
AnandhuA <anandhu1407@gmail.com>
2024-06-16 16:27:49 +0530

    some loading error fix

--- lib/presentation/Profile/widgets/profile_section_one.dart
+++ lib/presentation/Profile/widgets/profile_section_one.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:social_media/core/colors.dart';
 import 'package:social_media/core/size.dart';
 import 'package:social_media/presentation/CustomWidgets/custom_button.dart';
@@ -31,28 +33,55 @@ class ProfileSectionOne extends StatelessWidget {
               height: size.height * 0.23,
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage(
+                  image: CachedNetworkImageProvider(
                     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd8CLOYSV_UvfBrSm5loSGt0XaD7j_3lDRDw&s",
                   ),
                   fit: BoxFit.fill,
                 ),
               ),
-            )),
-            const Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: CircleAvatar(
-                backgroundColor: whiteColor,
-                radius: 65,
-                child: CircleAvatar(
-                  radius: 60,
-                  backgroundImage: NetworkImage(
-                    "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg",
-                  ),
-                ),
+              child: CachedNetworkImage(
+                imageUrl:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd8CLOYSV_UvfBrSm5loSGt0XaD7j_3lDRDw&s",
+                fit: BoxFit.cover,
+                placeholder: (context, url) {
+                  return Center(
+                    child: LoadingAnimationWidget.fourRotatingDots(
+                      color: greyColor,
+                      size: 30,
+                    ),
+                  );
+                },
+                errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
-            )
+            )),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: CircleAvatar(
+                  backgroundColor: whiteColor,
+                  radius: 65,
+                  child: CircleAvatar(
+                    radius: 60,
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          "https://imgv3.fotor.com/images/blog-cover-image/10-profile-picture-ideas-to-make-you-stand-out.jpg",
+                      imageBuilder: (context, imageProvider) => CircleAvatar(
+                        radius: 60,
+                        backgroundImage: imageProvider,
+                      ),
+                      placeholder: (context, url) => Center(
+                        child: LoadingAnimationWidget.fourRotatingDots(
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                      ),
+                      errorWidget: (context, url, error) => const Icon(
+                        Icons.error,
+                      ),
+                    ),
+                  ),
+                ))
           ],
         ),
         Text(
